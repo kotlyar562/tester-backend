c39162489c6b32c7ba8599d5c1de47cdb8b493e5
Andrey Kotlyar <kotlyar562@gmail.com>
2018-06-22 15:39:04 +0300

    Export Answer class

--- src/bases/models/__init__.py
+++ src/bases/models/__init__.py
@@ -1,3 +1,3 @@
 from . bases import QuestBase, Folder
 from . questions import Question, QuestionSettings
-from . answers import AnswerOneAsk, AnswerManyAsk, AnswerInput, AnswerOrdering 
+from . answers import Answer, AnswerOneAsk, AnswerManyAsk, AnswerInput, AnswerOrdering 
